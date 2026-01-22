02f3bc687bcfb489284a4395586b09a2392b4792
The 3-Minute Node <251228944+the3minutenode@users.noreply.github.com>
2026-01-22 11:37:49 -0500

    v0.4

--- comfy-adv-install.bat
+++ comfy-adv-install.bat
@@ -1,9 +1,11 @@
 @echo off&&cd /d "%~dp0"
 
-Title The 3-Minute Node ComfyUI Advanced Installer v0.3 260116
+Title The 3-Minute Node ComfyUI Advanced Installer v0.4 260122
 :: The 3-Minute Node Community Edition
 
 setlocal enabledelayedexpansion
+
+call :set_colors
 
 set "PYTHON_URL=https://www.python.org/ftp/python/3.13.9/python-3.13.9-embed-amd64.zip"
 set "TRITON_LIBS_URL=https://github.com/woct0rdho/triton-windows/releases/download/v3.0.0-windows.post1/python_3.13.2_include_libs.zip"
@@ -12,58 +14,58 @@ set "COMFY_DIR=%~dp0ComfyUI"
 set "PYTHON_EXE=%EMBED_DIR%\python.exe"
 
 :: rerequisites Check
-where git >nul 2>nul || (echo [+] Git not found. Please install Git for Windows. && pause && exit /b)
-where curl >nul 2>nul || (echo [+] Curl not found. Update your Windows or install Curl. && pause && exit /b)
+where git >nul 2>nul || (echo %RED%[+] Git not found. Please install Git for Windows.%RESET% && pause && exit /b)
+where curl >nul 2>nul || (echo %RED%[+] Curl not found. Update your Windows or install Curl.%RESET% && pause && exit /b)
 
-echo [+] Creating Directories...
+echo %CYAN%[+] Creating Directories...%RESET%
 if not exist "%EMBED_DIR%" mkdir "%EMBED_DIR%"
 
-echo [+] Downloading Python 3.13 Embeddable...
+echo %CYAN%[+] Downloading Python 3.13 Embeddable...%RESET%
 if not exist "python_embed.zip" curl --ssl-no-revoke -L -o python_embed.zip %PYTHON_URL%
 
-echo [+] Extracting Python...
+echo %CYAN%[+] Extracting Python...%RESET%
 powershell -Command "Expand-Archive -Path 'python_embed.zip' -DestinationPath '%EMBED_DIR%' -Force"
 del python_embed.zip
 
-echo [+] Handling Python Include/Libs...
-echo [+] Downloading 3.13.2 headers/libs for Triton...
+echo %CYAN%[+] Handling Python Include/Libs...%RESET%
+echo %CYAN%[+] Downloading 3.13.2 headers/libs for Triton...%RESET%
 curl --ssl-no-revoke -L -o "python_libs.zip" %TRITON_LIBS_URL%
-echo [+] Merging headers and libraries into %EMBED_DIR%...
+echo %CYAN%[+] Merging headers and libraries into %EMBED_DIR%...
 powershell -Command "Expand-Archive -Path 'python_libs.zip' -DestinationPath '%EMBED_DIR%' -Force"
 del python_libs.zip
 
-echo [+] Configuring Python Paths...
+echo %CYAN%[+] Configuring Python Paths...%RESET%
 powershell -Command "(Get-Content '%EMBED_DIR%\python313._pth') -replace '#import site', 'import site' | Set-Content '%EMBED_DIR%\python313._pth'"
 powershell -Command "$content = Get-Content '%EMBED_DIR%\python313._pth'; '../ComfyUI', $content | Set-Content '%EMBED_DIR%\python313._pth'"
 
-echo [+] Downloading pip...
+echo %CYAN%[+] Downloading pip...%RESET%
 curl --ssl-no-revoke -L -o "%EMBED_DIR%\get-pip.py" https://bootstrap.pypa.io/get-pip.py
 "%PYTHON_EXE%" "%EMBED_DIR%\get-pip.py" --no-warn-script-location
 del "%EMBED_DIR%\get-pip.py"
 
-echo [+] Installing PyTorch CUDA 13.0...
+echo %CYAN%[+] Installing PyTorch CUDA 13.0...%RESET%
 "%PYTHON_EXE%" -m pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu130
 
-echo [+] Installing Specialized Dependencies...
-echo [+] Installing Insightface...
+echo %CYAN%[+] Installing Specialized Dependencies...%RESET%
+echo %CYAN%[+] Installing Insightface...%RESET%
 "%PYTHON_EXE%" -m pip install https://github.com/Gourieff/Assets/raw/main/Insightface/insightface-0.7.3-cp313-cp313-win_amd64.whl
 
-echo [+] Installing Kokoro-ONNX...
+echo %CYAN%[+] Installing Kokoro-ONNX...%RESET%
 "%PYTHON_EXE%" -m pip install kokoro-onnx==0.4.4
 
-echo [+] Installing Triton for Windows...
+echo %CYAN%[+] Installing Triton for Windows...%RESET%
 "%PYTHON_EXE%" -m pip install triton-windows
 
-echo [+] Installing SageAttention...
+echo %CYAN%[+] Installing SageAttention...%RESET%
 "%PYTHON_EXE%" -m pip install https://github.com/woct0rdho/SageAttention/releases/download/v2.2.0-windows.post4/sageattention-2.2.0+cu130torch2.9.0andhigher.post4-cp39-abi3-win_amd64.whl
 
-echo [+] Cloning ComfyUI...
+echo %CYAN%[+] Cloning ComfyUI...%RESET%
 if not exist "%COMFY_DIR%" git clone https://github.com/comfyanonymous/ComfyUI.git "%COMFY_DIR%"
 
-echo [+] Installing ComfyUI Base Dependencies...
+echo %CYAN%[+] Installing ComfyUI Base Dependencies...%RESET%
 "%PYTHON_EXE%" -m pip install -r "%COMFY_DIR%\requirements.txt"
 
-echo [+] Installing Custom Nodes...
+echo %CYAN%[+] Installing Custom Nodes...%RESET%
 set "NODES=https://github.com/ltdrdata/ComfyUI-Manager https://github.com/rgthree/rgthree-comfy https://github.com/yolain/ComfyUI-Easy-Use https://github.com/kijai/ComfyUI-KJNodes https://github.com/crystian/ComfyUI-Crystools https://github.com/city96/ComfyUI-GGUF https://github.com/Fannovel16/comfyui_controlnet_aux https://github.com/ltdrdata/ComfyUI-Impact-Pack https://github.com/ltdrdata/ComfyUI-Impact-Subpack https://github.com/numz/ComfyUI-SeedVR2_VideoUpscaler https://github.com/cubiq/ComfyUI_IPAdapter_plus https://github.com/stavsap/comfyui-kokoro"
 
 cd /d "%COMFY_DIR%\custom_nodes"
@@ -71,15 +73,15 @@ for %%i in (%NODES%) do (
     set "URL=%%i"
     for /f "delims=" %%a in ("%%i") do set "FOLDER_NAME=%%~na"
     echo.
-    echo [+] Processing: !FOLDER_NAME!
+    echo %GREEN%[+] Processing: !FOLDER_NAME!%RESET%
     if not exist "!FOLDER_NAME!" (
         git clone !URL!
     )
     if exist "!FOLDER_NAME!\requirements.txt" (
 		if "!FOLDER_NAME!"=="comfyui-kokoro" (
-            echo [+] Skipping requirements for comfyui-kokoro
+            echo %YELLOW%[+] Skipping requirements for comfyui-kokoro%RESET%
         ) else (
-            echo [+] Installing requirements for !FOLDER_NAME!...
+            echo %GREEN%[+] Installing requirements for !FOLDER_NAME!...%RESET%
             "%PYTHON_EXE%" -m pip install -r "!FOLDER_NAME!\requirements.txt"
         )
     )
@@ -88,7 +90,7 @@ for %%i in (%NODES%) do (
 cd /d "%~dp0"
 
 echo.
-echo [+] Making run.bat...
+echo %CYAN%[+] Making run.bat...%RESET%
 (
 echo @echo off
 echo python_embeded\python.exe -s ComfyUI\main.py --windows-standalone-build --disable-api-nodes --use-sage-attention
@@ -96,7 +98,7 @@ echo pause
 ) > run.bat
 
 echo.
-echo [+] Making update.bat...
+echo %CYAN%[+] Making update.bat...%RESET%
 (
 echo @echo off
 echo cd /d "%%~dp0ComfyUI"
@@ -109,9 +111,9 @@ echo echo [+] Done.
 echo pause
 ) > update.bat
 
-:: Extra Model Paths Setup
+:: extra model paths setup
 echo.
-echo [+] Would you like to link an existing ComfyUI model folder?
+echo %CYAN%[+] Would you like to link an existing ComfyUI model folder?%RESET%
 set /p "LINK_MODELS=Enter 'y' for Yes or press Enter to skip:"
 
 if /i "%LINK_MODELS%"=="y" (
@@ -121,7 +123,7 @@ if /i "%LINK_MODELS%"=="y" (
     for /f "usebackq delims=" %%I in (`powershell -NoProfile -ExecutionPolicy Bypass -Command "Add-Type -AssemblyName System.Windows.Forms; $f = New-Object System.Windows.Forms.FolderBrowserDialog; $f.Description = 'Select your existing Model Folder (contains checkpoints, loras, etc)'; if($f.ShowDialog() -eq 'OK'){ $f.SelectedPath } "`) do set "USER_PATH=%%I"
 
     if defined USER_PATH (
-        echo [+] Path selected: !USER_PATH!
+        echo %GREEN%[+] Path selected: !USER_PATH!%RESET%
 
         :: Create the YAML file. We use forward slashes to avoid escape issues in YAML.
         set "ESCAPED_PATH=!USER_PATH:\=/!"
@@ -162,14 +164,25 @@ if /i "%LINK_MODELS%"=="y" (
 			echo     wav2vec2: wav2vec2
         ) > "%COMFY_DIR%\extra_model_paths.yaml"
 
-        echo [+] extra_model_paths.yaml created successfully.
+        echo %GREEN%[+] extra_model_paths.yaml created successfully.%RESET%
     ) else (
-        echo [+] No folder selected. Skipping...
+        echo %YELLOW%[+] No folder selected. Skipping...%RESET%
     )
 )
 
 echo.
-echo [+] Setup Complete.
-echo [+] Start: python_embeded\python.exe -s ComfyUI\main.py --windows-standalone-build
-
+echo %CYAN%[+] Setup Complete.%RESET%
+echo %CYAN%[+] Start: python_embeded\python.exe -s ComfyUI\main.py --windows-standalone-build%RESET%
 pause
+exit /b
+
+:: subroutines
+
+:set_colors
+for /f "delims=" %%a in ('powershell -command "[char]27"') do set "ESC=%%a"
+set "RESET=%ESC%[0m"
+set "GREEN=%ESC%[92m"
+set "CYAN=%ESC%[96m"
+set "RED=%ESC%[91m"
+set "YELLOW=%ESC%[93m"
+exit /b
